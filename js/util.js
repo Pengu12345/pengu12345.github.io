2f5366a73d96daa4b3252c89cbb1de0315bc5f1f
Pengu12345 <swanremacle@hotmail.fr>
2020-07-18 20:32:32 +0200

    Added menu and everything

--- js/util.js
+++ js/util.js
@@ -1,5 +1,32 @@
 var listImg = ['dave-peng.png', 'snap-peng.jpg', 'fez-peng.png'];
 
+function init() {
+
+}
+
+function openTab(evt, tabId) {
+  console.log("clicked on button " + evt + " to open " + tabId);
+
+  var leftChoices = document.getElementsByClassName("leftChoices");
+  var tabContent  = document.getElementsByClassName("tabContent");
+
+  console.log(leftChoices);
+  console.log(tabContent);
+ //Hide all the contents first
+ for(var i=0; i<tabContent.length; i++) {
+   tabContent[i].style.display = "none";
+ }
+
+ //Remove all active tab choices
+ for(var i=0; i<leftChoices.length; i++) {
+   leftChoices[i].className = leftChoices[i].className.replace(" active","");
+ }
+
+ document.getElementById(tabId).style.display = "block";
+ evt.path[0].className += " active";
+
+}
+
 function outputImages(path, col) {
 
   for(var i=0; i<listImg.length; i++) {
